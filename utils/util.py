4f91aa12d1581b80d19029d6629b1038869e72d2
Giuseppe Rizzo <giuse.rizzo@gmail.com>
2020-09-08 18:16:58 +0200

    Update util.py

---
+++ utils/util.py
@@ -0,0 +1,4 @@
+# bash command to prepare the 3column format necessary to be processed by the conlleval script
+# cat mlang.conll.test | cut -d' '  -f2 > column
+# paste -d' ' /tmp/mlang.conll.test /tmp/colmn > /tmp/mlang.conll.test.3col
+# perl /tmp/conlleval.pl < /tmp/mlang.conll.test.3col 
